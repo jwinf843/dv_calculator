bdf1e610965d93c7d1e7965dd6fd74bcba7a2c7d
Jordan Winfery <jwinf843@gmail.com>
2020-05-15 00:26:40 +0000

    Initialized project, added in all planetary bodies and moons for future reference

---
+++ bodies.py
@@ -0,0 +1,70 @@
+kerbol = {
+    'moons': None,
+}
+
+moho = {
+    'moons': None,
+    
+}
+
+gilly = {
+    'moons': True, 
+}
+
+eve = {
+    'moons': [gilly],
+}
+
+mün = {
+    'moons': True,
+}
+
+minmus = {
+    'moons': True,
+}
+
+kerbin = {
+    'moons': [mün, minmus],
+}
+
+ike = {
+    'moons': True,
+}
+
+duna = {
+    'moons': [ike],
+}
+
+dres = {
+    'moons': None,
+}
+
+laythe = {
+    'moons': True,
+}
+
+vall = {
+    'moons': True,
+}
+
+tylo = {
+    'moons': True, 
+}
+
+bop = {
+    'moons': True
+}
+
+pol = {
+    'moons': True
+}
+
+jool = {
+    'moons': [laythe, vall, tylo, bop, pol],
+}
+
+eeloo = {
+    'moons': None
+}
+
+bodies = [kerbol, moho, eve, kerbin, duna, dres, jool, eeloo]
